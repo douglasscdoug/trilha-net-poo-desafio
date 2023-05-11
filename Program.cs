8bbcaaa973e2abd8bb9efde5ab0320009534264e
douglasscdoug <douglasscdoug@gmail.com>
2023-05-11 20:40:17 -0300

    Desafio de projeto emplementado com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("555-1234", "Tijolão", "123jj458", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone");
+Iphone iphone = new Iphone("555-4321", "I12", "abc1234", 64);
+iphone.Numero = "555-6666";
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
